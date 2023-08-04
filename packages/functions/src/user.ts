fd7cc2152493e245a59101a53fecf4b9548abb5f
Justin Jaeger <jjustinjaeger@gmail.com>
2023-08-04 18:20:25 -0400

    add return types for user endpoints

--- packages/functions/src/user.ts
+++ packages/functions/src/user.ts
@@ -2,7 +2,6 @@ import { dbWrapper } from './helper/wrapper';
 import { type User } from './types/models';
 import { type ApiResponse } from './types/responses';
 
-// returns User
 export const get = dbWrapper(
   async ({ db }): Promise<ApiResponse<Partial<User>>> => {
     return {
@@ -12,26 +11,32 @@ export const get = dbWrapper(
   }
 );
 
-// returns User[]
-export const list = dbWrapper(async ({ db }) => {
-  return {
-    statusCode: 200
-  };
-});
+export const list = dbWrapper(
+  async ({ db }): Promise<ApiResponse<Partial<User[]>>> => {
+    return {
+      statusCode: 200,
+      data: []
+    };
+  }
+);
 
-// returns User[]
-export const listFollowings = dbWrapper(async ({ db }) => {
-  return {
-    statusCode: 200
-  };
-});
+export const listFollowings = dbWrapper(
+  async ({ db }): Promise<ApiResponse<Partial<User[]>>> => {
+    return {
+      statusCode: 200,
+      data: []
+    };
+  }
+);
 
-// returns User[]
-export const listFollowers = dbWrapper(async ({ db }) => {
-  return {
-    statusCode: 200
-  };
-});
+export const listFollowers = dbWrapper(
+  async ({ db }): Promise<ApiResponse<Partial<User[]>>> => {
+    return {
+      statusCode: 200,
+      data: []
+    };
+  }
+);
 
 export const post = dbWrapper(async ({ db }) => {
   return {
