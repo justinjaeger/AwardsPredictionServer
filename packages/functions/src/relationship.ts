e646ead405db132c9bb006336813c84450cdc054
Justin Jaeger <jjustinjaeger@gmail.com>
2023-08-14 23:59:49 -0400

    feat: relationship/get

--- packages/functions/src/relationship.ts
+++ packages/functions/src/relationship.ts
@@ -1,11 +1,22 @@
+import { ObjectId } from 'mongodb';
 import { dbWrapper } from './helper/wrapper';
+import { type Relationship } from './types/models';
 
-// returns Relationship (could be used to see if you have a relationship with a user)
-export const get = dbWrapper(async ({ db }) => {
-  return {
-    statusCode: 200
-  };
-});
+// used to see if you have a relationship with a user
+export const get = dbWrapper<{}, Relationship | null>(
+  async ({ db, params: { followingUserId, followedUserId } }) => {
+    const relationship = await db
+      .collection<Relationship>('relationships')
+      .findOne({
+        followingUserId: new ObjectId(followingUserId),
+        followedUserId: new ObjectId(followedUserId)
+      });
+    return {
+      statusCode: 200,
+      data: relationship
+    };
+  }
+);
 
 export const post = dbWrapper<{ followedUserId: string }>(
   async ({
