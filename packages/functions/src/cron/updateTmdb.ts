d1444630e9b3ebc0d74f6859bb70da2e00f58d48
Justin Jaeger <jjustinjaeger@gmail.com>
2023-08-29 00:59:00 -0400

    update updateTmdb cron func

--- packages/functions/src/cron/updateTmdb.ts
+++ packages/functions/src/cron/updateTmdb.ts
@@ -1,27 +1,39 @@
-import { type UpdateResult } from 'mongodb';
+import { type FindOptions, type Filter, type UpdateResult } from 'mongodb';
 import Tmdb from 'src/helper/tmdb';
 import { dbWrapper } from 'src/helper/wrapper';
-import { type Movie } from 'src/types/models';
+import { type Person, type Movie } from 'src/types/models';
 
+/**
+ * Updates all movie and person info, which is derived from tmdb
+ * Just do it for movies this year and later
+ */
 export const handler = dbWrapper(async ({ db }) => {
-  // Get all movies where the year is either null or gte to the current year
+  const filter: Filter<Movie | Person> = {
+    $or: [
+      { year: { $gte: new Date().getFullYear() } },
+      { year: { $e: null } } // movies with undefined year SHOULD be null when created - double check
+    ]
+  };
+  const options: FindOptions<Movie | Person> = {
+    projection: { _id: 1, tmdbId: 1 }
+  };
   const movies = await db
     .collection<Movie>('movies')
-    .find(
-      {
-        $or: [
-          { year: { $gte: new Date().getFullYear() } },
-          { year: { $e: null } } // movies with undefined year SHOULD be null when created - double check
-        ]
-      },
-      { projection: { _id: 1, tmdbId: 1 } }
-    )
+    .find(filter, options)
+    .toArray();
+  const persons = await db
+    .collection<Person>('persons')
+    .find(filter, options)
     .toArray();
 
   // get updated tmdb data (note: do not promise.all, it will break tmdb)
   const tmdbMovieData = movies.map(async (movie) => {
-    const res = await Tmdb.getMovieAsDbType(movie.tmdbId);
-    return res.data;
+    const { data } = await Tmdb.getMovieAsDbType(movie.tmdbId);
+    return data;
+  });
+  const tmdbPersonData = persons.map(async (person) => {
+    const { data } = await Tmdb.getPersonAsDbType(person.tmdbId);
+    return data;
   });
 
   // update db movies with the new tmdb data
@@ -42,7 +54,26 @@ export const handler = dbWrapper(async ({ db }) => {
       );
     }
   }
+  const updatePersonRequests: Array<Promise<UpdateResult<Person>>> = [];
+  for (let i = 0; i < persons.length; i++) {
+    const person = persons[i];
+    const newTmdbData = tmdbPersonData[i];
+    if (newTmdbData) {
+      updatePersonRequests.push(
+        db.collection<Person>('persons').updateOne(
+          {
+            _id: person._id
+          },
+          {
+            $set: newTmdbData
+          }
+        )
+      );
+    }
+  }
+
   await Promise.all(updateMovieRequests);
+  await Promise.all(updatePersonRequests);
 
   return {
     statusCode: 200
