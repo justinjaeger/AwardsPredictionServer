b9cde64192a049574bd016a9daa3577dccefbb74
Justin Jaeger <jjustinjaeger@gmail.com>
2023-08-30 03:13:18 -0400

    feat: prep amplify data

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,3 +1,16 @@
+//@ts-nocheck
+const { 
+    AmplifyCategory, 
+    AmplifyContender, 
+    AmplifyMovie, 
+    AmplifyPerson, 
+    AmplifyPrediction, 
+    AmplifyPredictionSet, 
+    AmplifyRelationship, 
+    AmplifySong, 
+    AmplifyUser,
+} = require("./types/amplifyApi");
+
 const dotenv = require('dotenv');
 dotenv.config();
 const { ObjectId } = require("mongodb");
@@ -16,13 +29,95 @@ const connectMongoDB = require("./helpers/connectMongoDB.ts");
 //     name: string;
 // }
 
+/**
+ * THE PLAN
+ * I'm going to copy over tables from aws to mongodb
+ * user -> users
+ * relationship -> relationships
+ * event/category -> events
+ * predictionSet/prediction -> predictionSets
+ * contender -> contenders
+ * movie -> movies
+ * person -> people
+ * song -> songs
+ * - for movie/person those can be filled out with their own cron func as long as tmdbId is there
+ * 
+ * categoryupdatelogs
+ * - doesn't correspond to anything
+ * - but since it's for history/calendar, just let it be blank for now. Then when users go and predict it will get filled out
+ * 
+ * Do not copy over any history.
+ * Not even the community predictions.
+ * - The cron func should just generate that for us based on current user predictions
+ * 
+ * DOING RELATIONSHIPS
+ * - I might make a legacy_id field on each table so that I can keep track of what was migrated
+ * - I may also have to create an object that associates previous id with new ids
+ * - Take users for example. I'm going to put all the new user info into mongodb.
+ * but the id is going to be unique and new, and after creating, I'll have to harness it.
+ * In the future, the old id can reference the new id.
+ * Then, when it comes to relationship, I'll have to do a lookup, what was the old id and what's its' new id? Then create the relationship like that.
+ * - What about prediction sets? Those exist in a few different places, and write to a few different tables.
+ * I'll just create the predictionset, then I'll go back and add to that user field by referencing the old id.
+ * 
+ * How I can run this multiple times
+ * - It won't create the thing if the same values already exist
+ * - but what if a user changes their name n stuff?
+ * - We have to use their ID and read from the MONGODB tables, and see does a user exist who has this aws_id?
+ * - If so, update their data, don't create new data.
+ * - Should enable us to run as many times as we want without overwriting data.
+ * - Maybe there's some "check if exists and update" func we can write?
+ * 
+ * What about OLD DUPLICATES???
+ * - I'm def gonna need to elimnate them during this migration,
+ * - but what I actually should do is run the deletion scripts first, then run this
+ * - if a duplicate comes up, Mongodb unique indexes should catch it, then we can go and run that amplify script
+ */
+
+const userTable = "User-jrjijr2sgbhldoizdkwwstdpn4-prod"
+const relationshipTable = 'Relationship-jrjijr2sgbhldoizdkwwstdpn4-prod'
+const eventTable = 'Event-jrjijr2sgbhldoizdkwwstdpn4-prod'
+const categoryTable = "Category-jrjijr2sgbhldoizdkwwstdpn4-prod"
+const contenderTable = 'Contender-jrjijr2sgbhldoizdkwwstdpn4-prod'
+const movieTable = 'Movie-jrjijr2sgbhldoizdkwwstdpn4-prod'
+const personTable = 'Person-jrjijr2sgbhldoizdkwwstdpn4-prod'
+const songTable = 'Song-jrjijr2sgbhldoizdkwwstdpn4-prod'
+const predictionTable = 'Prediction-jrjijr2sgbhldoizdkwwstdpn4-prod'
+const predictionSetTable = 'PredictionSet-jrjijr2sgbhldoizdkwwstdpn4-prod'
+
 async function handler() {
     const dynamodb = await connectDynamoDB();
     const mongodb = await connectMongoDB();
+
+    const getTableItems = async <AmplifyModel>(tableName: string) =>{
+        return (await dynamodb.send((new ScanCommand({ TableName: tableName })))).Items
+            .map((item)=>{
+                const itemKeys = Object.keys(item);
+                const itemValues = Object.values(item);
+                const itemEntries = itemKeys.map((key, index)=>{
+                    const val = itemValues[index];
+                    const str = val.S;
+                    const number = Number(val.N);
+                    if (str === 'TRUE') return [key, true];
+                    if (str === 'FALSE') return [key, false];
+                    return [key, (number || str)];
+                })
+                return Object.fromEntries(itemEntries);
+            }) as AmplifyModel[];
+    }
     
-    const scanCommand = new ScanCommand({ TableName: 'User-jrjijr2sgbhldoizdkwwstdpn4-prod' });
-    const response = await dynamodb.send(scanCommand);
-    console.log('dynamodb',response.Items[0])
+    // get all dynamodb items
+    const userItems = await getTableItems<AmplifyUser>(userTable)
+    const relationshipItems = await getTableItems<AmplifyRelationship>(relationshipTable)
+    const eventItems = await getTableItems<AmplifyUser>(eventTable)
+    const categoryItems = await getTableItems<AmplifyCategory>(categoryTable)
+    const contenderItems = await getTableItems<AmplifyContender>(contenderTable)
+    const movieItems = await getTableItems<AmplifyMovie>(movieTable)
+    const personItems = await getTableItems<AmplifyPerson>(personTable)
+    const songItems = await getTableItems<AmplifySong>(songTable)
+    const predictionItems = await getTableItems<AmplifyPrediction>(predictionTable)
+    const predictionSetItems = await getTableItems<AmplifyPredictionSet>(predictionSetTable)
+    console.log(contenderItems[0])
 
     // const users = mongodb.collection<User>("users");
     // const result = await users.find({}).toArray();
@@ -37,8 +132,6 @@ async function handler() {
     //   .collection('test')
     //   .find({ $text: { $search: 'hi' } }).toArray();
     // console.log('searchRes', searchRes);
-    
-    console.log('done!')
 }
 
 handler();
---
+++ scripts/types/amplifyApi.ts
@@ -0,0 +1,259 @@
+enum AmplifyUserRole {
+  ADMIN = "ADMIN",
+  TESTER = "TESTER",
+  USER = "USER",
+}
+
+type AmplifyUser = {
+  __typename: "User",
+  id: string,
+  email: string,
+  oauthId?: string | null,
+  username?: string | null,
+  name?: string | null,
+  bio?: string | null,
+  image?: string | null,
+  role: AmplifyUserRole,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifyPredictionSet = {
+  __typename: "PredictionSet",
+  id: string,
+  userId: string,
+  user: AmplifyUser,
+  eventId: string,
+  event: Event,
+  categoryId: string,
+  category: AmplifyCategory,
+  type?: AmplifyPredictionType | null,
+  comment?: string | null,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifyEvent = {
+  __typename: "Event",
+  id: string,
+  categories?: null,
+  awardsBody: AmplifyAwardsBody,
+  year: number,
+  nominationDateTime?: string | null,
+  winDateTime?: string | null,
+  status?: AmplifyEventStatus | null,
+  predictionSets?: null,
+  historyPredictions?: null,
+  liveAt?: string | null,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifyCategory = {
+  __typename: "Category",
+  id: string,
+  eventId: string,
+  event: Event,
+  name: AmplifyCategoryName,
+  type: AmplifyCategoryType,
+  phase?: AmplifyContenderAccolade | null,
+  lockTime?: string | null,
+  predictionSets?: null,
+  historyPredictions?: null,
+  createdAt: string,
+  updatedAt: string,
+};
+
+enum AmplifyCategoryName {
+  PICTURE = "PICTURE",
+  DIRECTOR = "DIRECTOR",
+  ACTOR = "ACTOR",
+  ACTRESS = "ACTRESS",
+  SUPPORTING_ACTOR = "SUPPORTING_ACTOR",
+  SUPPORTING_ACTRESS = "SUPPORTING_ACTRESS",
+  ORIGINAL_SCREENPLAY = "ORIGINAL_SCREENPLAY",
+  ADAPTED_SCREENPLAY = "ADAPTED_SCREENPLAY",
+  SCREENPLAY = "SCREENPLAY",
+  INTERNATIONAL = "INTERNATIONAL",
+  ANIMATED = "ANIMATED",
+  DOCUMENTARY = "DOCUMENTARY",
+  EDITING = "EDITING",
+  CINEMATOGRAPHY = "CINEMATOGRAPHY",
+  PRODUCTION_DESIGN = "PRODUCTION_DESIGN",
+  COSTUMES = "COSTUMES",
+  MAKEUP = "MAKEUP",
+  VISUAL_EFFECTS = "VISUAL_EFFECTS",
+  SOUND = "SOUND",
+  SCORE = "SCORE",
+  SONG = "SONG",
+  SHORT_ANIMATED = "SHORT_ANIMATED",
+  SHORT_DOCUMENTARY = "SHORT_DOCUMENTARY",
+  SHORT_LIVE_ACTION = "SHORT_LIVE_ACTION",
+  ENSEMBLE = "ENSEMBLE",
+  COMEDY_PICTURE = "COMEDY_PICTURE",
+  COMEDY_ACTOR = "COMEDY_ACTOR",
+  COMEDY_ACTRESS = "COMEDY_ACTRESS",
+  ACTION_PICTURE = "ACTION_PICTURE",
+  YOUNG_ACTOR = "YOUNG_ACTOR",
+  RISING_STAR = "RISING_STAR",
+  DEBUT = "DEBUT",
+  FIRST_SCREENPLAY = "FIRST_SCREENPLAY",
+  BRITISH_PICTURE = "BRITISH_PICTURE",
+  ANIMATED_PERFORMANCE = "ANIMATED_PERFORMANCE",
+  BLOCKBUSTER = "BLOCKBUSTER",
+  ACTING_ACHIEVEMENT = "ACTING_ACHIEVEMENT",
+  FEMALE_DIRECTOR = "FEMALE_DIRECTOR",
+  MALE_DIRECTOR = "MALE_DIRECTOR",
+  INDIE_PICTURE = "INDIE_PICTURE",
+  BREAKTHROUGH = "BREAKTHROUGH",
+}
+
+
+enum AmplifyCategoryType {
+  FILM = "FILM",
+  PERFORMANCE = "PERFORMANCE",
+  SONG = "SONG",
+}
+
+
+enum AmplifyContenderAccolade {
+  WINNER = "WINNER",
+  NOMINEE = "NOMINEE",
+  SHORTLISTED = "SHORTLISTED",
+}
+
+type AmplifyHistoryPredictionSet = {
+  __typename: "HistoryPredictionSet",
+  id: string,
+  userId: string,
+  user: AmplifyUser,
+  eventId: string,
+  event: Event,
+  categoryId: string,
+  category: AmplifyCategory,
+  predictions?: null,
+  type?: AmplifyPredictionType | null,
+  comment?: string | null,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifyHistoryPrediction = {
+  __typename: "HistoryPrediction",
+  id: string,
+  historyPredictionSetId: string,
+  contenderId: string,
+  contender: AmplifyContender,
+  categoryId: string,
+  category: AmplifyCategory,
+  ranking: number,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifyContender = {
+  __typename: "Contender",
+  id: string,
+  categoryId: string,
+  category: AmplifyCategory,
+  eventId: string,
+  event: Event,
+  movieId: string,
+  movie: AmplifyMovie,
+  personId?: string | null,
+  person?: AmplifyPerson | null,
+  songId?: string | null,
+  song?: AmplifySong | null,
+  visibility?: AmplifyContenderVisibility | null,
+  accolade?: AmplifyContenderAccolade | null,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifyMovie = {
+  __typename: "Movie",
+  id: string,
+  tmdbId: number,
+  studio?: string | null,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifyPerson = {
+  __typename: "Person",
+  id: string,
+  tmdbId: number,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifySong = {
+  __typename: "Song",
+  id: string,
+  movieId: string,
+  movie: AmplifyMovie,
+  title: string,
+  artist: string,
+  createdAt: string,
+  updatedAt: string,
+};
+
+enum AmplifyContenderVisibility {
+  HIDDEN = "HIDDEN",
+  VISIBLE = "VISIBLE",
+}
+
+
+enum AmplifyPredictionType {
+  WIN = "WIN",
+  NOMINATION = "NOMINATION",
+}
+
+
+enum AmplifyAwardsBody {
+  ACADEMY_AWARDS = "ACADEMY_AWARDS",
+  GOLDEN_GLOBES = "GOLDEN_GLOBES",
+  CRITICS_CHOICE = "CRITICS_CHOICE",
+  BAFTA = "BAFTA",
+  HCA = "HCA",
+  PGA = "PGA",
+  SAG = "SAG",
+  DGA = "DGA",
+  WGA = "WGA",
+  ADG = "ADG",
+  MAKEUP_GUILD = "MAKEUP_GUILD",
+  CDG = "CDG",
+  ASC = "ASC",
+  MPSE = "MPSE",
+}
+
+
+enum AmplifyEventStatus {
+  NOMS_STAGING = "NOMS_STAGING",
+  NOMS_LIVE = "NOMS_LIVE",
+  WINS_STAGING = "WINS_STAGING",
+  WINS_LIVE = "WINS_LIVE",
+  ARCHIVED = "ARCHIVED",
+}
+
+type AmplifyPrediction = {
+  __typename: "Prediction",
+  id: string,
+  predictionSetId: string,
+  contenderId: string,
+  contender: AmplifyContender,
+  ranking: number,
+  createdAt: string,
+  updatedAt: string,
+};
+
+type AmplifyRelationship = {
+  __typename: "Relationship",
+  id: string,
+  followedUserId: string,
+  followedUser: AmplifyUser,
+  followingUserId: string,
+  followingUser: AmplifyUser,
+  createdAt: string,
+  updatedAt: string,
+};
