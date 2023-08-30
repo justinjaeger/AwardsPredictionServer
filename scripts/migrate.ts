6b603d4df6fd9e2fd4566798a3764734a8e15829
Justin Jaeger <jjustinjaeger@gmail.com>
2023-08-29 22:22:39 -0400

    fix: script files

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,34 +1,44 @@
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 dotenv.config();
-import { ObjectId } from "mongodb";
-import { ScanCommand } from "@aws-sdk/client-dynamodb";
+const { ObjectId } = require("mongodb");
+const { ScanCommand } = require("@aws-sdk/client-dynamodb");
 // @ts-ignore
-import connectDynamoDB from "./helpers/connectDynamoDB.ts";
-// @ts-ignore
-import connectMongoDB from "./helpers/connectMongoDB.ts";
+const connectDynamoDB = require("./helpers/connectDynamoDB.ts");
+const connectMongoDB = require("./helpers/connectMongoDB.ts");
 
 /**
  * Documentation, all dynamodb commands:
  * https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/dynamodb/
  */
 
-interface User {
-    _id: ObjectId;
-    name: string;
-}
+// interface User {
+//     _id: ObjectId;
+//     name: string;
+// }
 
 async function handler() {
     const dynamodb = await connectDynamoDB();
     const mongodb = await connectMongoDB();
     
     const scanCommand = new ScanCommand({ TableName: 'User-jrjijr2sgbhldoizdkwwstdpn4-prod' });
-    const response = await dynamodb.send(scanCommand) as { Count: number, Items: any[], ScannedCount: number };
-    console.log('dynamodb',Object.keys(response))
+    const response = await dynamodb.send(scanCommand);
+    console.log('dynamodb',response.Items[0])
 
-    const users = mongodb.collection<User>("users");
-    const result = await users.find({}).toArray();
-    console.log('mongodb', result)
+    // const users = mongodb.collection<User>("users");
+    // const result = await users.find({}).toArray();
+    // console.log('mongodb', result)
+
+    // const a = await mongodb
+    //   .collection('test').indexes();
+    //   console.log('a',a)
+
+
+    // const searchRes = await mongodb
+    //   .collection('test')
+    //   .find({ $text: { $search: 'hi' } }).toArray();
+    // console.log('searchRes', searchRes);
     
+    console.log('done!')
 }
 
 handler();
