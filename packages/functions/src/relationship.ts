4c7cea50adb1a612dd6c99ed76c2203706f02e8c
Justin Jaeger <jjustinjaeger@gmail.com>
2023-09-28 00:32:00 -0400

    feat: make creating/deleting relationships also update user fields

--- packages/functions/src/relationship.ts
+++ packages/functions/src/relationship.ts
@@ -22,34 +22,104 @@ export const get = dbWrapper<{}, Relationship | null>(
 export const post = dbWrapper<
   { followedUserId: string },
   string // sends back the new relationship id
->(async ({ db, payload: { followedUserId }, authenticatedUserId }) => {
+>(async ({ db, client, payload: { followedUserId }, authenticatedUserId }) => {
   if (!authenticatedUserId) {
     return SERVER_ERROR.Unauthenticated;
   }
 
-  const relationship = await db
+  const createRelationshipRequest = db
     .collection<Relationship>('relationships')
     .insertOne({
       followedUserId: new ObjectId(followedUserId),
       followingUserId: new ObjectId(authenticatedUserId)
     });
 
+  const incrementFollowingCountRequest = db
+    .collection('users')
+    .updateOne(
+      { _id: new ObjectId(authenticatedUserId) },
+      { $inc: { followingCount: 1 } }
+    );
+
+  const incrementFollowerCountRequest = db
+    .collection('users')
+    .updateOne(
+      { _id: new ObjectId(followedUserId) },
+      { $inc: { followerCount: 1 } }
+    );
+
+  const session = client.startSession();
+  let relationshipId;
+  try {
+    session.startTransaction();
+    const res = await Promise.all([
+      createRelationshipRequest,
+      incrementFollowingCountRequest,
+      incrementFollowerCountRequest
+    ]);
+    relationshipId = res[0].insertedId.toString();
+    await session.commitTransaction();
+  } catch {
+    await session.abortTransaction();
+    return {
+      ...SERVER_ERROR.Error,
+      message: `Error creating relationship`
+    };
+  } finally {
+    await session.endSession();
+  }
+
   return {
     statusCode: 200,
-    data: relationship.insertedId.toString()
+    data: relationshipId
   };
 });
 
 export const remove = dbWrapper<{ followedUserId: string }, {}>(
-  async ({ db, payload: { followedUserId }, authenticatedUserId }) => {
+  async ({ db, client, payload: { followedUserId }, authenticatedUserId }) => {
     if (!authenticatedUserId) {
       return SERVER_ERROR.Unauthenticated;
     }
 
-    await db.collection<Relationship>('relationships').deleteOne({
-      followedUserId: new ObjectId(followedUserId),
-      followingUserId: new ObjectId(authenticatedUserId)
-    });
+    const deleteRelationshipRequest = db
+      .collection<Relationship>('relationships')
+      .deleteOne({
+        followedUserId: new ObjectId(followedUserId),
+        followingUserId: new ObjectId(authenticatedUserId)
+      });
+
+    const decrementFollowingCountRequest = db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(authenticatedUserId) },
+        { $inc: { followingCount: -1 } }
+      );
+
+    const decrementFollowerCountRequest = db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(followedUserId) },
+        { $inc: { followerCount: -1 } }
+      );
+
+    const session = client.startSession();
+    try {
+      session.startTransaction();
+      await Promise.all([
+        deleteRelationshipRequest,
+        decrementFollowingCountRequest,
+        decrementFollowerCountRequest
+      ]);
+      await session.commitTransaction();
+    } catch {
+      await session.abortTransaction();
+      return {
+        ...SERVER_ERROR.Error,
+        message: `Error removing relationship`
+      };
+    } finally {
+      await session.endSession();
+    }
 
     return {
       statusCode: 200
