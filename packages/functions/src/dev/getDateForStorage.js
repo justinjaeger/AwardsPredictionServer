61423efd456358c70b248b2991f12c200c6bbc32
Justin Jaeger <jjustinjaeger@gmail.com>
2024-01-07 15:45:52 -0500

    feat: add getDateForStorage func for development

---
+++ packages/functions/src/dev/getDateForStorage.js
@@ -0,0 +1,26 @@
+/**
+ * To run:
+ * node packages/functions/src/dev/getDateForStorage.js
+ *
+ * Helpful for getting dates based on EST date/times
+ * and logging the correct date in UTC (which should be stored)
+ */
+const EST_TIME = '8:00:00'; // if >12, then will be PM
+const EST_DATE = 'Jan 23 2024';
+const date = new Date(`${EST_DATE} ${EST_TIME}`);
+
+const invdate = new Date(
+  date.toLocaleString('en-US', {
+    timeZone: 'EST'
+  })
+);
+
+const diff = date.getTime() - invdate.getTime();
+
+const utcDate = new Date(date.getTime() - diff); // needs to substract
+
+// confirm that values are correct:
+console.log('Confirm this is correct:');
+console.log(utcDate.toString()); // 08:00:00 GMT+0500 (Eastern Universal Time)
+console.log('Paste this in mongodb:');
+console.log(utcDate); // 2024-01-23T13:00:00.000Z
