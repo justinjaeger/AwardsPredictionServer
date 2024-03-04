f59c3454b1f7576fd081436b81f30b48f9330737
Justin Jaeger <jjustinjaeger@gmail.com>
2024-03-03 22:38:31 -0500

    fix: update song key

--- packages/functions/src/helper/getSongKey.ts
+++ packages/functions/src/helper/getSongKey.ts
@@ -1,2 +1,8 @@
-export const getSongKey = (movieTmdbId: number, title: string) =>
-  `${movieTmdbId}-${title}`;
+/**
+ * Purpose is to not let there be duplicate song entries
+ * So if a user enters a song title slightly differently, it will be the same key
+ */
+export const getSongKey = (movieTmdbId: number, title: string) => {
+  const strippedTitle = title.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return `${movieTmdbId}-${strippedTitle}`;
+};
